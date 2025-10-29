debfc7d5f48f7184f2ae218a3832e41808164c9e
Emmanuel0085566-github <emmanuelniyi1997@gmail.com>
2025-10-29 17:18:33 +0100

    Cleaned Main Branch

--- routes/report.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { download } from '../controllers/report.controller.js';
-
-const router = Router();
-router.get('/download', download);
-export default router;
