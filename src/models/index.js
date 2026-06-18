da3805e9ac0f37c5fa0ed04045dc0057b96618fb
Emmanuel0085566-github <emmanuelniyi1997@gmail.com>
2026-06-18 20:36:46 +0100

    fix: register all models in index.js and add associations

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,9 @@
 import User from "./user.model.js";
 import BusinessProfile from "./business.profile.js";
+import Transaction from "./transaction.model.js";
+import IncomeExpense from "./income.expense.model.js";
+import TaxRecord from "./tax.record.model.js";
+import Reminder from "./reminder.model.js";
 
 User.hasOne(BusinessProfile, {
   foreignKey: "userId",
@@ -11,4 +15,45 @@ BusinessProfile.belongsTo(User, {
   as: "user",
 });
 
-export { User, BusinessProfile };
+// User associations
+User.hasMany(Transaction, {
+  foreignKey: "user_id",
+  as: "transactions",
+});
+
+Transaction.belongsTo(User, {
+  foreignKey: "user_id",
+  as: "user",
+});
+
+User.hasMany(IncomeExpense, {
+  foreignKey: "user_id",
+  as: "incomeExpenses",
+});
+
+IncomeExpense.belongsTo(User, {
+  foreignKey: "user_id",
+  as: "user",
+});
+
+User.hasMany(TaxRecord, {
+  foreignKey: "user_id",
+  as: "taxRecords",
+});
+
+TaxRecord.belongsTo(User, {
+  foreignKey: "user_id",
+  as: "user",
+});
+
+User.hasMany(Reminder, {
+  foreignKey: "user_id",
+  as: "reminders",
+});
+
+Reminder.belongsTo(User, {
+  foreignKey: "user_id",
+  as: "user",
+});
+
+export { User, BusinessProfile, Transaction, IncomeExpense, TaxRecord, Reminder };
